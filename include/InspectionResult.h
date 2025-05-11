3f04bcee4bb7b5c9043129e1189c2eaa1183dc35
lybahoang <157574648+lybahoang@users.noreply.github.com>
2025-05-11 10:36:54 +0700

    Update InspectionResult.h (Hoang)

--- include/InspectionResult.h
+++ include/InspectionResult.h
@@ -10,18 +10,15 @@ class InspectionResult
 protected:
     string title;               // The title of the paper.
     bool inspectionResult;      // The inspection result.
-    vector<string> notes;       // The details of the inspection result.
     virtual void setInspectionResult() = 0;
 public:
     // Default constructor.
     InspectionResult()
-        { title = "blank";
-          inspectionResult = false; }
+        { inspectionResult = false; }
     
     // Constructor.
     InspectionResult(const string &t)
-        { title = t;
-          inspectionResult = false; }
+        { inspectionResult = false; }
 
     // Accessor functions.
     string getTitle() const
@@ -29,24 +26,10 @@ public:
     
     bool getInspectionResult() const
         { return inspectionResult; }
-    
-    vector<string> getNotes() const
-        { return notes; }
-
-    void displayNotes() const
-        {
-            for (int count = 0; count < notes.size(); count++)
-            {
-                cout << "   " << notes[count] << endl;
-            }
-        }
 
     // Mutator functions.
     void setTitle(const string &newTitle)
         { title = newTitle; }
-    
-    void addNote(const string &newNote)
-        { notes.push_back(newNote); }
 
     // Destructor.
     virtual ~InspectionResult() { };
