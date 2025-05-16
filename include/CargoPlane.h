4b208da2209a5eebe326d3b0c622382c7fc885a9
lybahoang <157574648+lybahoang@users.noreply.github.com>
2025-05-16 15:36:49 +0700

    Planes Header files

---
+++ include/CargoPlane.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "Plane.h"
+class CargoPlane :
+    public Plane
+{
+private:
+    int payloadCapacity; // in kg
+public:
+    //constructor
+    CargoPlane();
+
+    // override function
+    int maxPayLoad() const override;
+    int passenger_Capacity() const override;
+
+
+    // setter
+    void setPayload(int payloadCapacity);
+    
+};
+
+//operator overloading
+    ostream& operator<<(ostream& os, const CargoPlane& plane);
+    istream& operator>>(istream& is, CargoPlane& plane);
