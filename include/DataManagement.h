24585eb47b0e54d60705b66d1e6f32c380fa5431
lybahoang <157574648+lybahoang@users.noreply.github.com>
2025-05-20 07:11:34 +0700

    Update DataManagement.h

--- include/DataManagement.h
+++ include/DataManagement.h
@@ -1,7 +1,6 @@
 #ifndef DATA_MANAGEMENT_H
 #define DATA_MANAGEMENT_H
 #include "PilotStandard.h"
-#include "PlaneSpecification.h"
 #include <vector>
 #include <string>
 #include <fstream>
@@ -13,17 +12,11 @@ class DataManagement
 private:
     // Pilot-related variable (Hoang)
     static vector<PilotStandard> pilotStandardArray;  
-    // Aircraft specification.  
-    static vector<PlaneSpecification> planeSpecArray;
-    // Airport.
+
 public:
     // Pilot-related functions (Hoang)
     static void loadPilotStandard(const string &fileName);
     static PilotStandard findPilotStandard(const string &model);
     static void displayPilotStandards(const string &model);
-
-    // Aircraft specification (Hoang).
-    static void loadPlaneSpecification(const string &fileName);
-    static PlaneSpecification findPlaneSpecification(const string &model);
 };
 #endif
