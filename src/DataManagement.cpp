70bed3b03c26570e328600d6dc4591a99d290901
lybahoang <157574648+lybahoang@users.noreply.github.com>
2025-05-20 07:06:13 +0700

    Create DataManagement class

---
+++ src/DataManagement.cpp
@@ -0,0 +1,245 @@
+#include "../include/DataManagement.h"
+#include "../include/StringManipulator.h"
+#include <sstream>
+
+// Define static member variables
+vector<PilotStandard> DataManagement::pilotStandardArray;    // Pilot-related variable (Hoang)
+vector<PlaneSpecification> DataManagement::planeSpecArray;
+
+
+// Function DataManagement::loadPilotStandard (Hoang)
+void DataManagement::loadPilotStandard(const string &fileName)
+{
+    // Open the file.
+    ifstream inputFile(fileName);
+    if (inputFile.fail())
+    {
+        cout << "ERROR: File named " << fileName << " not found.\n";
+        exit(EXIT_FAILURE);
+    }
+
+    // Variable to hold a line in the file.
+    string line;
+
+    // Read the pilot's standards and put it in the vector.
+    getline(inputFile, line);       // Skip the header line.
+
+    // Reading the file.
+    int index = 0;  // Index of the array.
+
+    while(getline(inputFile, line))
+    {
+        // Skip empty lines.
+        if (line.empty()) 
+            continue; 
+
+        // Create a PilotStandard object.
+        PilotStandard pilotStandard;
+
+        // Convert the line into a stream.
+        stringstream ss(line);
+        string token;
+
+        // Variable declaration.
+        string model;
+        string requiredType;
+        double minFlightHours;
+        double minHoursInCommand;
+        int minEnglish;
+        int minHealth;
+
+        // Read the model name.
+        getline(ss, model, ',');
+
+        // Read the minRequiredFlightHours.
+        getline(ss, token, ',');
+        minFlightHours = stoi(token);
+
+        // Read the minRequiredHoursInCommand.
+        getline(ss, token, ',');
+        minHoursInCommand = stoi(token);
+
+        // Read the requiredLicenseType.
+        getline(ss, requiredType, ',');
+
+        // Read the minRequiredEnglishLevel.
+        getline(ss, token, ',');
+        minEnglish = stoi(token);
+
+        // Read the minRequiredHealthStatus.
+        getline(ss, token, ',');
+        minHealth = stoi(token);
+
+        // Store the data of standards into the array.
+        try
+        {
+            pilotStandard.setModelName(model);
+            pilotStandard.setMinRequiredFlightHours(minFlightHours);
+            pilotStandard.setMinRequiredHoursInCommand(minHoursInCommand);
+            pilotStandard.setRequiredLicenseType(requiredType);
+            pilotStandard.setMinRequiredEnglishLevel(minEnglish);
+            pilotStandard.setRequiredHealthStatus(minHealth);
+            pilotStandardArray.push_back(pilotStandard);
+            // Increment the index.
+            index++;
+        }
+        catch (PilotStandard::InvalidModel)
+        {
+            cout << "LoadFileError: Pilot standards file contain invalid model.\n";
+            exit(EXIT_FAILURE);
+        }
+        catch (PilotStandard::InvalidHours)
+        {
+            cout << "LoadFileError: Pilot standards file contains invalid value for hours.\n";
+            exit(EXIT_FAILURE);
+        } 
+        catch (PilotStandard::InvalidEnglish)
+        {
+            cout << "LoadFileError: Pilot standards file contains invalid value for english level.\n";
+            exit(EXIT_FAILURE);
+        } 
+        catch (PilotStandard::InvalidHealth)
+        {
+            cout << "LoadFileError: Pilot standards file contains invalid value for heath status.\n";
+            exit(EXIT_FAILURE);
+        } 
+        catch (PilotStandard::InvalidType)
+        {
+            cout << "LoadFileError: Pilot standards file contains invalid value for license type.\n";
+            exit(EXIT_FAILURE);
+        } 
+    }
+
+    inputFile.close();
+}
+
+// Function DataManagement::findPilotStandard (Hoang)
+PilotStandard DataManagement::findPilotStandard(const string &model)
+{
+    // Remove spaces and capitalize the argument.
+    string processedModel = StringManipulator::removeSpaces(model);
+    processedModel = StringManipulator::capitalize(processedModel);
+
+    // Define a PilotStandard object.
+    PilotStandard pilotStandard;
+    bool isFound = false;
+    for (int count = 0; count < pilotStandardArray.size(); count++)
+    {
+        if (pilotStandardArray[count].getModelName() == processedModel)
+        {
+            isFound = true;
+            pilotStandard = pilotStandardArray[count];
+            break;
+        }
+    }
+
+    return pilotStandard;
+}
+
+
+// Function DataManagement::displayPilotStandards (Hoang)
+void DataManagement::displayPilotStandards(const string &model)
+{
+    // Search for the pilot standard corresponding to the model.
+    PilotStandard standard = findPilotStandard(model);
+    // Display the pilot standard.
+    cout << standard << endl;
+}
+
+// Function DataManagement::loadPlaneSpecification (Hoang)
+void DataManagement::loadPlaneSpecification(const string &fileName)
+{
+    // Open the file.
+    ifstream inputFile(fileName);
+    if (inputFile.fail())
+    {
+        cout << "ERROR: File named " << fileName << " not found.\n";
+        exit(EXIT_FAILURE);
+    }
+
+    // Variable to hold a line in the file.
+    string line;
+
+    // Read the pilot's standards and put it in the vector.
+    getline(inputFile, line);       // Skip the header line.
+
+    // Reading the file.
+    while(getline(inputFile, line))
+    {
+        // Skip empty lines.
+        if (line.empty()) 
+            continue; 
+
+        // Convert the line into a stream.
+        stringstream ss(line);
+        string token;
+
+        // Variable declaration.
+        string model;
+        double maxPayload;
+        int maxSeats;
+
+        // Read the model name.
+        getline(ss, model, ',');
+
+        // Read the maxPayload.
+        getline(ss, token, ',');
+        maxPayload = stod(token);
+
+        // Read the maxSeats
+        getline(ss, token, ',');
+        maxSeats = stoi(token);
+
+        // Store the data of standards into the array.
+        try
+        {
+            // Create an AircraftSpecification object.
+            PlaneSpecification specification(model, maxPayload, maxSeats);
+            planeSpecArray.push_back(specification);
+        }
+        catch(PlaneSpecification::InvalidModel m)
+        {
+            cout << "ERROR: File contains invalid data for model name.\n";
+            inputFile.close();
+            exit(EXIT_FAILURE);
+        }
+        catch (PlaneSpecification::InvalidPayload p)
+        {
+            cout << "ERROR: File contains invalid data for payload.\n";
+            inputFile.close();
+            exit(EXIT_FAILURE);
+        }
+        catch(PlaneSpecification::InvalidSeats s)
+        {
+            cout << "ERROR: File contains invalid data for maximum seats.\n";
+            inputFile.close();
+            exit(EXIT_FAILURE);
+        }
+    }
+    inputFile.close();
+}
+
+
+// Function DataMangement::findPlaneSpecification (Hoang)
+PlaneSpecification DataManagement::findPlaneSpecification(const string &model)
+{
+    // Remove spaces and capitalize the argument.
+    string processedModel = StringManipulator::removeSpaces(model);
+    processedModel = StringManipulator::capitalize(processedModel);
+
+    // Define a PilotStandard object.
+    PlaneSpecification specification;
+    bool isFound = false;
+
+    for (int count = 0; count < pilotStandardArray.size(); count++)
+    {
+        if (planeSpecArray[count].getModelName() == processedModel)
+        {
+            isFound = true;
+            specification = planeSpecArray[count];
+            break;
+        }
+    }
+
+    return specification;
+}
